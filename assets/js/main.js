818afadc273f6a0367047cd93fa9f4996d81384b
tsubakikajitani <89886736+tsubakikajitani@users.noreply.github.com>
2021-09-21 22:31:43 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: '星座',
+          q1: '星',
         },
         stage2: {
           q1: 'あたま',
@@ -26,7 +26,7 @@ const app = Vue.createApp({
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'くすり',
+          q1: 'まくら',
           // q2: 'かかか',
           // q3: 'ききき',
         }
